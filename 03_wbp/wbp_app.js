aba32fa38fceefb255e13a56e1f8697a0f1c3336
lynchbyte <61684154+lynchbyte@users.noreply.github.com>
2020-03-07 17:09:37 +1100

    Update wbp_app.js

--- 03_wbp/wbp_app.js
+++ 03_wbp/wbp_app.js
@@ -50,7 +50,7 @@ var uniforms;
 
 var cameraTarget
 
-const soundClick = new Howl({ src: ['03_media/Audio/LV-HTIS Beeps Simple 03.wav'], volume: 1, });
+const soundClick = new Howl({ src: ['03_media/Audio/Beep.wav'], volume: 1, });
 
 init();
 animate();
